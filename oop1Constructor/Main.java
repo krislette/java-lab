24e79cf406aefaf70d48a1484156b56230089da3
Krislette <acellekrislette@gmail.com>
2023-11-04 03:10:25 +0800

    created a new class

--- oop1Constructor/Main.java
+++ oop1Constructor/Main.java
@@ -12,22 +12,26 @@ public class Main {
         // From now on, I have to practice compiling EVERY SINGLE TIME.
 
         // Si constructor yung gumagawa/nagi-initialize ng object, hence the name "constructor".
-        System.out.print("Name: ");
-        String strName = input.nextLine();
+        // System.out.print("Name: ");
+        // String strName = input.nextLine();
 
-        System.out.print("Price: ");
-        float fltPrice = input.nextFloat();
+        // System.out.print("Price: ");
+        // float fltPrice = input.nextFloat();
 
-        Product prod = new Product(strName, fltPrice);   
+        // Product prod = new Product(strName, fltPrice);   
         
-        System.out.println(prod.fltPrice);
-        System.out.println(prod.strName);
+        // System.out.println(prod.fltPrice);
+        // System.out.println(prod.strName);
 
-        Character characterRegina = new Character("Regina", "Hi bitch!", 100, 50, 10);
-        characterRegina.talkTo("Acelle");
+        // Character characterRegina = new Character("Regina", "Hi bitch!", 100, 50, 10);
+        // characterRegina.talkTo("Acelle");
 
-        Person Acelle = new Person("Acelle", "Rosales", 'F', 18);
-        System.out.println(Acelle.strLastName);
+        // Person Acelle = new Person("Acelle", "Rosales", 'F', 18);
+        // System.out.println(Acelle.strLastName);
+
+        Student Acelle = new Student("Acelle", "Rosales", 2, "BSCS", 5, 95.5f, 98.8f);
+        Acelle.introduceSelf();
+        Acelle.evaluateGrade();
     }
 
 }
---
+++ oop1Constructor/Student.java
@@ -0,0 +1,47 @@
+package oop1Constructor;
+
+public class Student {
+    
+    String strFName, strLName, strCourse;
+    int intYear, intSection;
+    float fltMidtermGrade, fltFinalGrade;  
+
+    Student(String strFName, String strLName, int intYear, String strCourse, int intSection, float fltMidtermGrade, float fltFinalGrade) {
+        this.strFName = strFName;
+        this.strLName = strLName;
+        this.intYear = intYear;
+        this.strCourse = strCourse;
+        this.intSection = intSection;
+        this.fltMidtermGrade = fltMidtermGrade;
+        this.fltFinalGrade = fltFinalGrade;
+    }
+
+    void introduceSelf() {
+        System.out.println("Full name: " + strFName + " " + strLName);
+        System.out.println("Course, year, and section: " + strCourse + " " + intYear + "-" + intSection);
+    }
+
+    void evaluateGrade() {
+        System.out.println("Midterm Grade: " + fltMidtermGrade);
+        System.out.println("Final Grade: " + fltFinalGrade);
+        
+        // Getting the average.
+        float fltAverageGrade = (fltMidtermGrade + fltFinalGrade) / 2;
+        System.out.println("Average Grade: " + fltAverageGrade);
+
+        if (fltAverageGrade >= 100) {
+            System.out.println("Invalid grade.");
+        } else if (fltAverageGrade >= 98) {
+            System.out.println("With Highest Honors.");
+        } else if (fltAverageGrade >= 95) {
+            System.out.println("With High Honors.");
+        } else if (fltAverageGrade >= 90) {
+            System.out.println("With Honors.");
+        } else if (fltAverageGrade >= 75) {
+            System.out.println("Passed.");
+        } else {
+            System.out.println("Failed.");
+        }
+    }
+
+}
